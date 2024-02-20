a1d61b1c80ede519fdf85c1197915ecf139af309
Brian <briantechdom@gmail.com>
2024-02-20 15:50:27 -0800

    more cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
    return (
       <div>      
-        <h1 className="read-the-docs">
+        <h1 className="head-text">
         Click on the Vite and React logos to learn more
         </h1>
       </div>
