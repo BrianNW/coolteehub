d098a3c2f9cd6e0681f5bb070be8541ecf3db188
Brian <briantechdom@gmail.com>
2024-05-29 15:33:59 -0700

    Add initial state values

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,12 @@ import {proxy} from 'valtio';
 
 // call this as a function and provide intiial object
 const state = proxy({
-
+    intro: true,
+    color: '#EFBD48',
+    isLogoTexture: true,
+    isFullTexture: false,
+    logoDecal: './threejs.png',
+    fullDecal: './threejs.png'
 })
 
 export default state; 
